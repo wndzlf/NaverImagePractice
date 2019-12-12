6e7c2dcb134e4e1d48fae31e8098862b98e88ab2
조중현 <jjoonghyun.cho@navercorp.com>
2019-12-12 18:15:12 +0900

    Step2 적용

--- Step2/TableViewPractice/NaverImageTableViewCell.swift
+++ Step2/TableViewPractice/NaverImageTableViewCell.swift
@@ -12,6 +12,7 @@ class NaverImageTableViewCell: UITableViewCell {
     
     @IBOutlet weak var naverImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var naverImageHeightConstraint: NSLayoutConstraint!
     
     var httpTask: URLSessionDataTask?
     var indexPathRow: Int = 0
@@ -32,11 +33,22 @@ class NaverImageTableViewCell: UITableViewCell {
                     return
                 }
                 DispatchQueue.main.async {
+                    self?.setImageHeight(image.size)
                     self?.naverImage.image = image
                     self?.imageDicDelegate?.updateImageDictioinary(link: imageURLString, value: image)
                 }
             }
         }
+    }
+    
+    private func setImageHeight(_ imageSize: CGSize) {
+        var ratio = imageSize.height / imageSize.width
+        if ratio > 1.3 {
+            ratio = 1.3
+        } else if ratio < 0.7 {
+            ratio = 0.7
+        }
+        naverImageHeightConstraint.constant = naverImageHeightConstraint.constant * ratio
     }
     
     override func awakeFromNib() {
